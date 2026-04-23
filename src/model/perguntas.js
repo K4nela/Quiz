492a7877c4f0a7fa7c36d20100d1d14353004416
K4nela <mariaclara.clarinha1503@gmail.com>
2026-04-22 21:26:44 -0300

    adicionando criação de personagens e perguntas com opcoes (e seus pesos)

--- src/model/perguntas.js
+++ src/model/perguntas.js
@@ -1,38 +1,24 @@
-// Sugestão para o esquema de perguntas e respostas. Temos aqui um objeto perguntas que terá como atributos: uma string
+// Sugestão para o esquema de Pergunta e respostas. Temos aqui um objeto Pergunta que terá como atributos: uma string
 // para o texto da pergunta e uma lista de objetos, que conterão os seguintes atributos: uma string opção para receber o
 // texto da opção e a respectiva pontuação para os personagens
 
-//Aqui vcs fiquem a vontade pra modificar, não concordei tanto com essa ideia do chat nn... mas faz sentido
-const perguntas = [
-    {
-        pergunta: "[Pergunta]",
-        resposta: [
-            {
-                opcao1: "[Opcao1]",
-                pontos: {
-                    personagem1: 0,
-                    personagem2: 0,
-                    personagem3: 0
-                }
-            },
+//Aqui vocês fiquem a vontade para modificar, não concordei tanto com essa ideia do chat nn... mas faz sentido
 
-            {
-                opcao2: "[Opcao2]",
-                pontos: {
-                    personagem1: 0,
-                    personagem2: 0,
-                    personagem3: 0
-                }
-            },
-
-            {
-                opcao3: "[Opcao3]",
-                pontos: {
-                    personagem1: 0,
-                    personagem2: 0,
-                    personagem3: 0
-                }
-            }
-        ]
+export class Pergunta {
+    constructor(pergunta, opcoes) {
+        this.pergunta = pergunta;
+        this.opcao = opcoes;
     }
-]
+}
+
+export let perguntas = []
+
+export class Opcoes {
+
+    constructor(texto, peso1, peso2, peso3) {
+        this.texto = texto;
+        this.peso1 = peso1;
+        this.peso2 = peso2;
+        this.peso3 = peso3;
+    }
+}
--- src/model/personagens.js
+++ src/model/personagens.js
@@ -1,9 +1,12 @@
 //  Vamos usar objetos para criar os personagens. Facilitando o acesso às propriedades para as chamadas na página de
 //  resultados
 
-const personagens = {
-    name: "Nome",
-    description: "Descrição",
-    img: ".png",
-    numValue: 0
-};
+export class Personagem {
+
+    constructor (nome, description, img, numValue){
+        this.name = nome;
+        this.description = description;
+        this.img = img;
+        this.numValue = numValue;
+    }
+}
