1900e7ecba32f51cb5f1183fdc751aa717b6ff61
Rafael D <147335135+rafadari@users.noreply.github.com>
2026-04-22 21:35:43 -0300

    Update personagens.js

--- src/model/personagens.js
+++ src/model/personagens.js
@@ -1,9 +1,26 @@
 //  Vamos usar objetos para criar os personagens. Facilitando o acesso às propriedades para as chamadas na página de
 //  resultados
 
-const personagens = {
-    name: "Nome",
-    description: "Descrição",
+
+// Criei os persongens e denominei com o 1,2,3 pra ficar mais fácil de mexer depois, os .png não peguei ainda
+
+const personagens1 = {
+    name: "Naruto",
+    description: "Você é o Naruto, determinado e muito leal com quem você escolhe pra ter no seu caminho",
+    img: ".png",
+    numValue: 0
+};
+
+const personagens2 = {
+    name: "Sasuke",
+    description: "Você é o Sasuke, focado na própria evolução com uma personalidade forte.",
+    img: ".png",
+    numValue: 0
+};
+
+const personagens3 = {
+    name: "Sakura",
+    description: "Você é a Sakura, gentil e foca muito no bem estar das pessoas ao seu redor.",
     img: ".png",
     numValue: 0
 };
