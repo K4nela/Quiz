ae6643e63a2afb9bf1a0047ade35625ef8a41aa5
Maria Eduarda <eduardanjos03@gmail.com>
2026-04-23 19:17:42 -0300

    Remove comments
    Removed comments suggesting modifications to the Pergunta class structure.

--- src/model/perguntasModel.js
+++ src/model/perguntasModel.js
@@ -1,9 +1,3 @@
-// Sugestão para o esquema de Pergunta e respostas. Temos aqui um objeto Pergunta que terá como atributos: uma string
-// para o texto da pergunta e uma lista de objetos, que conterão os seguintes atributos: uma string opção para receber o
-// texto da opção e a respectiva pontuação para os personagens
-
-//Aqui vocês fiquem a vontade para modificar, não concordei tanto com essa ideia do chat nn... mas faz sentido
-
 export class Pergunta {
     constructor(pergunta, ...opcoes) {
         this.pergunta = pergunta;
