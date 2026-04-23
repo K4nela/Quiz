7f5f239367b5f64a0900e3f4d1bc4d5c576537d3
Maria Eduarda <eduardanjos03@gmail.com>
2026-04-23 19:19:05 -0300

    Remove comments
    Removed comments for clarity in the getPersonagem and resultado functions.

--- src/view/paginaResultsView.js
+++ src/view/paginaResultsView.js
@@ -1,12 +1,10 @@
 import {listaDePersonagens} from "../data/personagensData.js";
 import {pontuacaoTotal, resetQuiz} from "./paginaQuizView.js";
 
-//verificar qual personagem se parece mais com o resultado
 export function getPersonagem(pontuacaoTotal){
     return listaDePersonagens.find(personagem => pontuacaoTotal <= personagem.numValue);
 }
 
-//exibir as informações na tela
 export function resultado(pontuacaoTotal){
     const personagemFinal = getPersonagem(pontuacaoTotal);
     console.log(personagemFinal)
